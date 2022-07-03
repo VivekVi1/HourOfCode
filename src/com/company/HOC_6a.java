4655795db34afa437efe20cb506c51ab171474ce
VivekVi1 <vivek.vishwakarma1020@gmail.com>
2022-07-03 17:55:51 +0530

    eighth commit

--- src/com/company/HOC_6a.java
+++ src/com/company/HOC_6a.java
@@ -4,15 +4,15 @@ public class HOC_6a {
     public static void main(String[] args) {
         String a = "10000001";
         String b = "01111111";
-        int count = 0,result;
+        int count = 0, result;
         float temp;
         for (int i = 0; i < 8; i++) {
-            if(a.charAt(i)!=b.charAt(i)){
+            if (a.charAt(i) != b.charAt(i)) {
                 count++;
             }
         }
-        temp = (float)(count/8.0) * 100;
-        result = (int)Math.floor(temp);
+        temp = (float) (count / 8.0) * 100;
+        result = (int) Math.floor(temp);
         System.out.println(result);
     }
 }
--- src/com/company/HOC_6b.java
+++ src/com/company/HOC_6b.java
@@ -3,20 +3,21 @@ package com.company;
 import java.util.Scanner;
 
 public class HOC_6b {
-    static float clockAngle(String time){
+    static float clockAngle(String time) {
         String[] s = time.split(":");
         float hours = Integer.parseInt(s[0]);
         float min = Integer.parseInt(s[1]);
 
-        float hrDeg = (hours%12 + min/60)*30;
+        float hrDeg = (hours % 12 + min / 60) * 30;
         float minDeg = min * 6;
 
         float angle = Math.abs(hrDeg - minDeg);
-        if(angle > 180){
+        if (angle > 180) {
             angle = 360 - angle;
         }
         return angle;
     }
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
