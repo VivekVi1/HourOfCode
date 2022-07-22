705c9369fbe936030ff5a154a84b4cade97f12cc
VivekVi1 <vivek.vishwakarma1020@gmail.com>
2022-07-22 11:19:25 +0530

    second commit

---
+++ src/com/company/Arrays/Subset.java
@@ -0,0 +1,27 @@
+package com.company.Arrays;
+
+import java.util.HashSet;
+
+public class Subset {
+    static String isSubset(long[] a1, long[] a2, long n, long m) {
+        HashSet<Long> hset = new HashSet<>();
+
+        for(int i = 0; i < n;i++){
+            hset.add(a1[i]);
+        }
+        for(int i = 0; i < m;i++){
+            if(!hset.contains(a2[i]));{
+                return "No";
+            }
+        }
+        return "Yes";
+    }
+    public static void main(String[] args) {
+        long [] a1 = {11, 1, 13, 21, 3, 7};
+        long [] a2 = {11, 3, 7, 1};
+        long n = a1.length;
+        long m = a2.length;
+
+        System.out.println(isSubset(a1,a2,n,m));
+    }
+}
---
+++ src/com/company/Arrays/SymmetricPairs.java
@@ -0,0 +1,39 @@
+package com.company.Arrays;
+
+public class SymmetricPairs {
+    static void   findSymPairs(int [][]arr,int row){
+        for (int i = 0; i < row; i++) {
+            for (int j = i+1; j < row; j++) {
+                if (arr[i][0] == arr[j][1] && arr[i][1] == arr[j][0]){
+                    System.out.print("(");
+                    System.out.print(arr[i][0]);
+                    System.out.print(",");
+                    System.out.print(arr[i][1]);
+                    System.out.print(")");
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        //  Input: arr[] = {{11, 20}, {30, 40}, {5, 10}, {40, 30}, {10, 5}}
+        /*  Output: Following pairs have symmetric pairs
+                (30, 40)
+                (5, 10)
+        */
+
+        int[][] arr = new int[5][2];
+        arr[0][0] = 11;
+        arr[0][1] = 20;
+        arr[1][0] = 30;
+        arr[1][1] = 40;
+        arr[2][0] = 5;
+        arr[2][1] = 10;
+        arr[3][0] = 40;
+        arr[3][1] = 30;
+        arr[4][0] = 10;
+        arr[4][1] = 5;
+
+        findSymPairs(arr,5);
+    }
+}
